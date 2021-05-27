1c50f1b8abfbf7138008f652cc210d31ee21e290
aruna.va <aruna.va@media.net>
2021-05-27 18:54:57 +0530

    added delimiter option

--- src/js/components/MultiSelect/MultiSelect.js
+++ src/js/components/MultiSelect/MultiSelect.js
@@ -5,7 +5,7 @@ import { Box } from '../Box';
 import { Select } from '../Select';
 
 import { ColumnSelect } from './ColumnSelect';
-import { ValueLabelWithNumber } from './ValueLabelWithNumber';
+import { ValueLabelWithIcon } from './ValueLabelWithIcon';
 import { applyKey } from './utils';
 
 const MultiSelect = ({
@@ -31,21 +31,27 @@ const MultiSelect = ({
   renderEmptySelected,
   gridArea,
   validate,
+  size,
+  isOpenState,
+  isEnableOutSideClick,
+  shouldRenderInDrop,
+  placeholder,
+  delimiter,
   ...rest
 }) => {
-
   const [internalValue, updateInternalValue] = useState(valueProp);
-  const [
-    internalIsExcluded,
-    updateInternalIsExcluded,
-  ] = useState(isExcludedProp);
-  const [isOpen, updateIsOpen] = useState(false);
+  const [internalIsExcluded, updateInternalIsExcluded] = useState(
+    isExcludedProp,
+  );
+  const [isOpen, updateIsOpen] = useState(isOpenState || false);
   const [search, updateSearch] = useState('');
+  const [searchArray, updateSearchArray] = useState([]);
 
-  const isExcluded = withUpdateCancelButtons ? 
-  internalIsExcluded : isExcludedProp;
+  const isExcluded = withUpdateCancelButtons
+    ? internalIsExcluded
+    : isExcludedProp;
 
-  const value = withUpdateCancelButtons ? internalValue: valueProp;
+  const value = withUpdateCancelButtons ? internalValue : valueProp;
 
   useEffect(() => {
     if (!isOpen && withUpdateCancelButtons) {
@@ -67,13 +73,14 @@ const MultiSelect = ({
       updateInternalIsExcluded(isExcludedProp);
     }
     updateIsOpen(true);
-  }
+  };
 
   const onIncludeExclude = newValue => {
-    const updater = withUpdateCancelButtons ? 
-    updateInternalIsExcluded : onIncExcChange;
+    const updater = withUpdateCancelButtons
+      ? updateInternalIsExcluded
+      : onIncExcChange;
     updater(newValue);
-  }
+  };
 
   const onCancelClick = () => {
     onClose();
@@ -85,44 +92,81 @@ const MultiSelect = ({
       onIncExcChange(isExcluded);
     }
     updateIsOpen(false);
-  }
+  };
 
   const getValue = (index, array, param) => applyKey(array[index], param);
 
   const onSearchChange = searchInput => {
-    const escapedText = searchInput.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
+    const escapedText = searchInput.replace(/[-\\^$*+?.()[\]{}]/g, '\\$&');
+    if (delimiter) {
+      if (searchInput === '') {
+        updateSearchArray([]);
+      } else {
+        const escapedTextSplit = escapedText
+          .split(delimiter)
+          .map(item => item.trim());
+          updateSearchArray(escapedTextSplit);
+      }
+    }
     updateSearch(escapedText);
   };
 
   const getOptions = useCallback(() => {
-    if (!search) {
-      return options;
+    if (delimiter) {
+      if (searchArray.length === 0) {
+        return options;
+      }
+      return options.filter(item =>
+        searchArray.some(searchEl => {
+          const exp = new RegExp(`^${searchEl}$`, 'i');
+          return exp.test(item.label);
+        }),
+      );
+    } else {
+      if (!search) {
+        return options;
+      }
+      const exp = new RegExp(search, 'i');
+      return options.filter((item, index) =>
+        exp.test(getValue(index, options, labelKey)),
+      );
     }
-    const exp = new RegExp(search, 'i');
-    return options.filter((item, index) =>
-      exp.test(getValue(index, options, labelKey)),
-    );
-  }, [options, search])
+  }, [options, search]);
 
   const getOptionsNotMatchingSearch = useCallback(() => {
-    if (!search) {
-      return [];
+    if (delimiter) {
+      if (!searchArray.length) {
+        return [];
+      }
+      const filteredOptions = options.filter(
+        item =>
+          !searchArray.some(searchEl => {
+            const exp = new RegExp(`^${searchEl}$`, 'i');
+            return exp.test(item.label);
+          }),
+      );
+      return filteredOptions;
+    } else {
+      if (!search) {
+        return [];
+      }
+      const exp = new RegExp(search, 'i');
+      return options.filter(
+        (item, index) => !exp.test(getValue(index, options, labelKey)),
+      );
     }
-    const exp = new RegExp(search, 'i');
-    return options.filter((item, index) =>
-      !exp.test(getValue(index, options, labelKey)),
-    );
-  }, [options, search])
+  }, [options, search]);
 
   const onSelectValueChange = ({ value: newValue }) => {
     const valuesNotMatchingSearch = getOptionsNotMatchingSearch()
-    .filter((item, index, opt) => 
-    value.includes(getValue(index, opt, valueKey)))
-    .map((item, index, opt)=> getValue(index, opt, valueKey));
+      .filter((item, index, opt) =>
+        value.includes(getValue(index, opt, valueKey)),
+      )
+      .map((item, index, opt) => getValue(index, opt, valueKey));
 
-    const updater = withUpdateCancelButtons ? 
-    updateInternalValue : 
-    onValueChange;
+    const updater = withUpdateCancelButtons
+      ? updateInternalValue
+      : onValueChange;
     updater([...valuesNotMatchingSearch, ...newValue]);
   };
 
@@ -159,17 +203,13 @@ const MultiSelect = ({
   };
 
   const renderLabel = () => {
-    const getLabel = () => {
-      if (withInclusionExclusion && isExcluded) return 'Excluded';
-      if (withInclusionExclusion && isExcluded === false) return 'Included';
-      return 'Selected';
-    };
-
     return (
-      <ValueLabelWithNumber
-        value={getLabel()}
-        number={value.length}
-        color="brand"
+      <ValueLabelWithIcon
+        withInclusionExclusion={withInclusionExclusion}
+        isExcluded={isExcluded}
+        size={size}
+        placeholder={placeholder}
+        value={value}
       />
     );
   };
@@ -196,6 +236,9 @@ const MultiSelect = ({
         onSearch={onSearch}
         searchPlaceholder={searchPlaceholder}
         emptySearchMessage={emptySearchMessage}
+        isEnableOutSideClick={isEnableOutSideClick}
+        shouldRenderInDrop={shouldRenderInDrop}
+        size={size}
         {...rest}
       />
     </Box>
