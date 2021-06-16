0623dbfa116f1decec45a6ae8de5e2a6be3ed260
Saumik Sarkar <51847390+SaumikSarkar@users.noreply.github.com>
2021-06-16 17:07:37 +0530

    [Tooltip] Customizations support added for text (#45)
    Co-authored-by: saumik.s <saumik.s@media.net>

--- src/js/components/Tooltip/Tooltip.js
+++ src/js/components/Tooltip/Tooltip.js
@@ -4,10 +4,9 @@ import { ThemeContext } from 'styled-components';
 import { defaultProps } from '../../default-props';
 
 import { Box } from '../Box';
-import { Text } from '../Text';
 import { Drop } from '../Drop';
 
-import { ArrowWrap, Arrow } from './StyledTooltip';
+import { ArrowWrap, Arrow, StyledTooltipText } from './StyledTooltip';
 
 const Tooltip = forwardRef(
   ({ children, message, position = 'right', ...rest }, ref) => {
@@ -54,7 +53,9 @@ const Tooltip = forwardRef(
                 background={tooptip.background || 'dark-1'}
                 round={tooptip.round}
               >
-                <Text color={tooptip.color}>{message}</Text>
+                <StyledTooltipText color={tooptip.color} {...tooptip.text}>
+                  {message}
+                </StyledTooltipText>
               </Box>
             </ArrowWrap>
           </Drop>
